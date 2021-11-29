5c3908dd1554cdbd99c6947b7abffd9fbe7b5b24
EddyBautista <bautista.j.eddy@gmail.com>
2021-11-29 06:56:06 -0600

    set up node class and methods ;

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace linked_list_codwars
+{
+    public class Node
+    {
+        public int Data;
+        public Node Next;
+
+        public Node(int data)
+        {
+            this.Data = data;
+            this.Next = null;
+        }
+
+        public static Node Push(Node head, int data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Node BuildOneTwoThree()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+    }
+    class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
+}
